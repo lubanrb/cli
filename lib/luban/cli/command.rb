0f650de397b8fb2246f71a501df12ebd18b8408a
Chi Man Lei <chi.lei@laxino.com>
2015-04-04 18:54:46 +0800

    Show correct command chain between program name and synopsis when composing parser banner

--- lib/luban/cli/command.rb
+++ lib/luban/cli/command.rb
@@ -2,16 +2,29 @@ module Luban
   module CLI
     class Command < Base
       attr_reader :name
+      attr_reader :parent
 
-      def initialize(app, name, prefix: '__command_', &config_blk)
-        super
+      def initialize(app, name, parent: nil, prefix: '__command_', &config_blk)
         @name = name
+        @parent = parent
+        super(app, name, prefix: prefix, &config_blk)
       end
 
       protected
 
+      def command_chain
+        return @command_chain unless @command_chain.nil?
+        chain = [name]
+        next_parent = parent
+        while !next_parent.nil?
+          chain.unshift next_parent.name
+          next_parent = next_parent.parent
+        end
+        @command_chain = chain.join(' ')
+      end
+
       def compose_banner
-        "Usage: #{program_name} #{name} #{compose_synopsis}"
+        "Usage: #{program_name} #{command_chain} #{compose_synopsis}"
       end
     end
   end

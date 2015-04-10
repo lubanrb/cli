78df7fe42df08012ff63dcd1f3de7a673aae4d04
Chi Man Lei <chi.lei@laxino.com>
2015-04-10 08:34:23 +0800

    update test cases accordingly

--- spec/command_spec.rb
+++ spec/command_spec.rb
@@ -2,7 +2,9 @@ require_relative 'spec_helper'
 
 class TestApp
   def self.add_command(name)
-    Luban::CLI::Command.new(self, name)
+    Luban::CLI::Command.new(self, name) do
+      command :nested_cmd
+    end
   end
 end
 
@@ -13,9 +15,16 @@ describe Luban::CLI::Command do
 
   it "has a name" do
     @cmd.name.must_equal :test_cmd
+    @cmd.commands[:nested_cmd].name.must_equal :nested_cmd
   end
 
-  it "composes command synopsis with command name" do
+  it "has a parent" do
+    @cmd.parent.must_be_nil
+    @cmd.commands[:nested_cmd].parent.must_be_instance_of(Luban::CLI::Command)
+  end
+
+  it "composes command synopsis with a proper command chain" do
     @cmd.parser.banner.must_match /#{@cmd.program_name} #{@cmd.name}/
+    @cmd.commands[:nested_cmd].parser.banner.must_match /#{@cmd.program_name} #{@cmd.name} #{@cmd.commands[:nested_cmd].name}/
   end
 end

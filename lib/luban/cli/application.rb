f74eafce94a78ea4e4a7983e373094ed9202da6c
Chi Man Lei <chi.lei@laxino.com>
2015-10-19 14:11:12 +0800

    provide convenient method :start to create and run new instance of CLI application

--- lib/luban/cli/application.rb
+++ lib/luban/cli/application.rb
@@ -3,12 +3,23 @@ require 'pathname'
 module Luban
   module CLI
     class Application < Base
+      class << self
+        def start(class_name = nil, action_name = :run, **opts, &config_blk)
+          create(class_name, action_name, **opts, &config_blk).tap { |app| app.send(action_name) }
+        end
+
+        protected
+
+        def create(class_name, action_name = :run, **opts, &config_blk)
+          klass = class_name.nil? ? self : Object.const_get(class_name, false)
+          klass.new(action_name, **opts, &config_blk)
+        end
+      end
+
       attr_reader :rc
 
       def initialize(action_name = :run, **opts, &config_blk)
         super(self, action_name, **opts, &config_blk)
-        @rc = init_rc
-        validate
       end
 
       def rc_file
@@ -29,7 +40,9 @@ module Luban
 
       protected
 
-      def validate; end
+      def on_configure
+        @rc = init_rc
+      end
 
       def init_rc
         if rc_file_exists?
